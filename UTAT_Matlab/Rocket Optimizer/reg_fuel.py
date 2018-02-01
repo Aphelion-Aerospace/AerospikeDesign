b937af643c598a3df9b8223f94929e9b98db1ed6
Kevin Course <kevinlcourse@gmail.com>
2018-01-31 22:03:43 -0500

    Began creation of custom function, began filling in main_Recordbreaker.m constants with our values

--- UTAT_Matlab/Rocket Optimizer/reg_fuel.py
+++ UTAT_Matlab/Rocket Optimizer/reg_fuel.py
@@ -1,3 +1,13 @@
+rho_fuel = 950 
+L = 0.3048 #fuel core length CHECK
+r_fo = 0.068/2 # Inner combustion chamber radius
+a0 = 0.000155 # Regression rate coeff (m/s**2)
+n_reg = 0.5 # Regression rate exponent, FLUX EXP???
+MW_ox = 44.013 # Molecular weight/mass of N2O (kg/kmol)
+m_ox = #liquid ox mass in tank initial
+
+n = m_ox/MW_ox # moels of liquid ox
+
 r_port = r_fo - w #fuel port radius; r_fo = inner combustion chamber radius
 
 G_ox = m_dot_ox[i]/(np.pi*r_port**2) #ox mass flux
@@ -8,4 +18,4 @@ w = w - reg_rate*dt # w = initial fuel web thickness
 
 n = n + dn*dt # consume moles of ox
 
-m_f[i+1] = rho_fuel*L*np.pi*w*(2*r_fo-w)
+m_f[i+1] = rho_fuel*L*np.pi*w*(2*r_fo-w) # mass fuel
